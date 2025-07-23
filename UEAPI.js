0287e4deed74141a680c7c21f911c0f0443ec953
fengy <fonlylovey@163.com>
2025-07-23 11:10:36 +0800

    添加webui调用

--- UEAPI.js
+++ UEAPI.js
@@ -1,19 +1,31 @@
 
 import WebUI from "./WebUI.js";
 
+const StreamMode = {WEBUI: 0, PEERSTREAM: 1};
+
 export default class UE
 {
-    //peer 发送消息
-    static EmitMessage(jsonObject)
+    
+    static UseStreamMode = StreamMode.PEERSTREAM;
+
+    //在像素流页面初始化的时候必须调用，确定使用什么模式
+    static SetStreamMode(Mode)
     {
-        window.ps.emitMessage(jsonObject);
-        console.log("向UE发送数据", jsonObject);
+        UseStreamMode = Mode;
     }
 
-    //WebUI模式发送消息
-    static EmitWebUIMessage(funName, jsonObject)
+    //peer 发送消息
+    static EmitMessage(funName, jsonObject)
     {
-        WebUI.sendMessage(funName, jsonObject);
+        if(this.UseStreamMode == StreamMode.PEERSTREAM)
+        {
+            window.ps.emitMessage(jsonObject);
+        }
+        else
+        {
+            WebUI.sendMessage(funName, jsonObject);
+        }
+        console.log("向UE发送数据", jsonObject);
     }
 
     static EmitCommand(stringData)
@@ -23,7 +35,6 @@ export default class UE
             Data: {"Command": stringData}
         }
         UE.EmitMessage(jsonData);
-        UE.EmitWebUIMessage("ExecCommandline", jsonData);
     }
 
     //跳转到某个视点
@@ -34,17 +45,15 @@ export default class UE
             Data: {"Time": time, "ViewPointID": viewpointID}
         }
         UE.EmitMessage(jsonData);
-        UE.EmitWebUIMessage("ExecCommandline", jsonData);
     }
 
     //设置Actor的可见性
-    static SetActorVisible(isShow)
+    static SetActorVisible(actorName, isShow)
     {
         let jsonData = {
             Name: "SetActorVisible",
-            Data: {"Name": "Pipeline_2", "Visible":false}
+            Data: {"Name": actorName, "Visible":isShow}
         }
         UE.EmitMessage(jsonData);
-        UE.EmitWebUIMessage("ExecCommandline", jsonData);
     }
 }
